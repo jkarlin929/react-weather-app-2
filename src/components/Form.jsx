effc303738f85b85f6eca42fe64a682157209e77
jkarlin929 <osida11572@gmail.com>
2019-02-18 14:10:32 -0500

    input allows for text

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,22 +1,60 @@
 import React, { Component } from 'react';
-import fetchWeather from '../services/api-helpers';
+import WeatherData from './WeatherData'
 
 class Form extends Component {
   constructor() {
     super();
     this.state = {
-      weather: null
+      zip: '',
+      weather: null,
+      isLoading: true,
+      error: null
     };
+    this.handleChange = this.handleChange.bind(this)
   };
 
   async componentDidMount() {
-    await fetchWeather()
+    await this.fetchWeather();
+  };
+
+   fetchWeather(zip) {
+    const BASE_URL = `https://api.openweathermap.org/data/2.5/weather?zip=10029,us&units=imperial`;
+    const API_KEY = `8aac04ebc33f3c51fa258b707bf5bd1a`;
+    fetch(`${BASE_URL}&appid=${API_KEY}`)
+    .then(res => res.json())
+    .then(data =>
+    this.setState({
+        weather: null
+      })
+    )
+    .catch(error => this.setState({
+      error, isLoading: false
+    }));
   }
+
+  handleChange(e) {
+    console.log(e.target.value);
+    this.setState({
+      zip: e.target.value
+    })
+  }
+
+  // handleSubmit(e) {
+  //   e.preventDefault;
+  // }
 
 
 render() {
   return (
-    <h1>Form Component</h1>
+      <form action="GET">
+        <input
+        type="text"
+        placeholder="Enter ZipCode..."
+        value={this.state.zip}
+        onChange={this.handleChange}
+        />
+        <button>Submit</button>
+      </form>
     );
   };
 };
--- src/components/WeatherData.jsx
+++ src/components/WeatherData.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const WeatherData = props => (
-
+  <h1>Weather Data</h1>
 );
 
 export default WeatherData;
